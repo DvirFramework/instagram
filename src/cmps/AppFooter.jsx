aaa08f452a7967b1dcd089b20f93d869cd73bb5c
Dvir Dor <dvirdor123121@gmail.com>
2024-01-07 13:03:50 +0200

    frist commit

---
+++ src/cmps/AppFooter.jsx
@@ -0,0 +1,30 @@
+
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
+
+import { removeFromCart, checkout } from '../store/car.actions'
+import { UserMsg } from './UserMsg.jsx'
+
+export function AppFooter() {
+    const [isCartShown, setIsCartShown] = useState(false)
+    const cart = useSelector(storeState => storeState.carModule.cart)
+    const count = useSelector(storeState => storeState.userModule.count)
+    const cartTotal = cart.reduce((acc, car) => acc + car.price, 0)
+
+    async function onCheckout() {
+        try {
+            const score = await checkout(cartTotal)
+            showSuccessMsg(`Charged, your new score: ${score.toLocaleString()}`)
+        } catch(err) {
+            showErrorMsg('Cannot checkout')
+        }
+    }
+
+    return (
+        <footer >
+           
+            <UserMsg />
+        </footer>
+    )
+}
